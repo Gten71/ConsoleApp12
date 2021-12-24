3dae8bff6f8beb52f6ad0a270f45d63c26ef719a
User <User@DESKTOP-C7DGT0E>
2021-12-24 15:15:51 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+//5.2 базовый 13 вариант 
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static double sred(int[,] a, int b)
+        {
+            double sum = 0;
+            for (int i = 0; i < a.GetLength(1); i++)
+            {
+                sum += a[b, i];
+            }
+            double arif = sum / a.GetLength(1);
+            return arif;
+        }
+    }
+}
---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[7, 6]
+        {
+       { 1, 2, 3, 4, 5, 6 },
+       { -1, -2, -3, 4, 5, 6 },
+        { 7, 8, 9, -10, 1, -9 },
+        { -9, -8, -7, 11, -6, -5 },
+       { 5, 6, 4, -9, 1, -3 },
+        { 3, 5, 3, -7, 1, -2 },
+        { 2, 8, 9, 5, 1, -6 }
+        };
+            double[] mas = new double[7];
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                double sred = ClassLibrary1.Class1.sred(a, i);
+                mas[i] = sred;
+            }
+            foreach (double item in mas)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
